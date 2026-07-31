77754bf3249297307401dbc127c69ab8a44b6a59
Lebogang Ramathibela <116263263+leramathibela@users.noreply.github.com>
2026-07-31 22:48:11 +0200

    Delete app/lip directory

--- app/lip/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://ccntnagudznyiefmbbip.supabase.co'
-const supabaseKey = 'sb_publishable_73lBU_B7KvitoOJiZMzh-g_sRrmU_eg'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
