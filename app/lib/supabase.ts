a77c1d33c781263443305ee2cc0ab88628064504
Lebogang Ramathibela <116263263+leramathibela@users.noreply.github.com>
2026-07-31 22:59:31 +0200

    Update supabase.ts

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://ccntnagudznyiefmbbip.supabase.co'
-const supabaseKey = 'sb_publishable_73lBU_B7KvitoOJiZMzh-g_sRrmU_eg'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjbnRuYWd1ZHpueWllZm1iYmlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyNjE0NzMsImV4cCI6MjA5OTgzNzQ3M30.ibsntRsk-dnw9qr7Z-Vmdlon5lbkKLvtwTHTx-WX6O4'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
